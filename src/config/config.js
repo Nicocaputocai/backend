d4d3dfaee2f5e684d4c6ecd35105827b022e890d
nicocaputocai <nicolas_caputo@hotmail.com>
2022-11-09 15:04:35 -0300

    paso los datos de conexion de la db por dot env

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config({path:"./.env"})
+
 module.exports = {
     PORT: process.env.PORT || 4000,
-    DB: process.env.DB || 'mongodb+srv://desarrolloi:Lanus2030@cluster0.wug6n.mongodb.net/mapaLanus?retryWrites=true&w=majority'
+    DB: process.env.DB || `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wug6n.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
 }
