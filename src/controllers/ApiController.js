830b936e93c88565d5910f9d4c035da1d888dc96
nicocaputocai <nicolas_caputo@hotmail.com>
2023-12-13 15:37:38 -0300

    agregue ruta a api list

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -2,7 +2,8 @@ module.exports = {
     index: function (req,res){
         return res.status(200).json({
             local:{
-                api: 'http://localhost:4000/api'
+                api: 'http://localhost:4000/api',
+                "Lista de árboles": 'http://localhost:4000/api/trees'
             }
         })
     }
